bbf9c696ae14af209a32dc758830b758fb468748
ArturoDA <diazarcaarturo@gmail.com>
2018-11-14 02:21:48 -0500

    Arturo ArreglosSocio nro2

--- HOTEL/src/Arreglos/ArregloSocio.java
+++ HOTEL/src/Arreglos/ArregloSocio.java
@@ -3,20 +3,7 @@ import java.util.ArrayList;
 
 import Clases.Socio;
 public class ArregloSocio {
-	//Atributos privados
-	private ArrayList<Socio> socio;
-	//Constructor
-	public ArregloSocio() {
-		socio = new ArrayList<Socio>();
-		cargarSocio();
-	}
-	//Operaciones publicas basicas
-	public void adicionar(Socio x) {
-		socio.add(x);
-	}
-	public int tamaño() {
-		return socio.size();
-	}
+	
 	
 
 }
