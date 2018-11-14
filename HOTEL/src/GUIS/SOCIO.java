4f5ba72b9334176ef3556ca9b3f043ab5f706d28
ArturoDA <diazarcaarturo@gmail.com>
2018-11-14 05:20:23 -0500

    Arturo SocioGUI 14/11 05:20

--- HOTEL/src/GUIS/SOCIO.java
+++ HOTEL/src/GUIS/SOCIO.java
@@ -1,6 +1,7 @@
 package GUIS;
 
 import java.awt.BorderLayout;
+
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
@@ -8,6 +9,7 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -16,6 +18,14 @@ import javax.swing.JTextArea;
 import java.awt.Font;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+import Arreglos.ArregloSocio;
+import Clases.Socio;
+
+import java.awt.Color;
 
 public class SOCIO extends JDialog implements ActionListener, MouseListener {
 
@@ -28,16 +38,16 @@ public class SOCIO extends JDialog implements ActionListener, MouseListener {
 	private JTextField txtNombre;
 	private JTextField txtDni;
 	private JTextField txtApellido;
-	private JTextField txtCodigo;
-	private JTextField textField_4;
+	private JTextField txtCodigoSocio;
+	private JTextField txtTelefono;
 	private JButton btnAceptar;
-	private JButton btnListar;
+	private JButton btnAdicionar;
 	private JButton btnModificar;
-	private JButton btnConsultar;
 	private JButton btnEliminar;
-	private JScrollPane scrollPane;
-	private JTextArea textArea;
 	private JLabel label;
+	private JScrollPane scrollPane;
+	private JTable tblSocio;
+	private DefaultTableModel modelo;
 
 	/**
 	 * Launch the application.
@@ -103,62 +113,76 @@ public class SOCIO extends JDialog implements ActionListener, MouseListener {
 		contentPanel.add(txtApellido);
 		txtApellido.setColumns(10);
 		
-		txtCodigo = new JTextField();
-		txtCodigo.setEditable(false);
-		txtCodigo.setBounds(298, 11, 86, 20);
-		contentPanel.add(txtCodigo);
-		txtCodigo.setColumns(10);
+		txtCodigoSocio = new JTextField();
+		txtCodigoSocio.setEditable(false);
+		txtCodigoSocio.setBounds(298, 11, 86, 20);
+		contentPanel.add(txtCodigoSocio);
+		txtCodigoSocio.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setEditable(false);
-		textField_4.setBounds(296, 50, 86, 20);
-		contentPanel.add(textField_4);
-		textField_4.setColumns(10);
+		txtTelefono = new JTextField();
+		txtTelefono.setEditable(false);
+		txtTelefono.setBounds(296, 50, 86, 20);
+		contentPanel.add(txtTelefono);
+		txtTelefono.setColumns(10);
 		
 		btnAceptar = new JButton("ACEPTAR");
+		btnAceptar.setForeground(Color.BLACK);
 		btnAceptar.setBounds(295, 89, 89, 23);
 		contentPanel.add(btnAceptar);
 		
-		btnListar = new JButton("ADICIONAR");
-		btnListar.addActionListener(this);
-		btnListar.setBounds(559, 7, 111, 28);
-		contentPanel.add(btnListar);
+		btnAdicionar = new JButton("ADICIONAR");
+		btnAdicionar.addActionListener(this);
+		btnAdicionar.setBounds(559, 7, 111, 28);
+		contentPanel.add(btnAdicionar);
 		
 		btnModificar = new JButton("MODIFICAR");
 		btnModificar.setBounds(559, 46, 111, 32);
 		contentPanel.add(btnModificar);
 		
-		btnConsultar = new JButton("CONSULTAR");
-		btnConsultar.addActionListener(this);
-		btnConsultar.setBounds(559, 86, 111, 32);
-		contentPanel.add(btnConsultar);
-		
 		btnEliminar = new JButton("ELIMINAR");
-		btnEliminar.setBounds(559, 129, 111, 32);
+		btnEliminar.setBounds(559, 89, 111, 32);
 		contentPanel.add(btnEliminar);
-		
-		scrollPane = new JScrollPane();
-		scrollPane.setBounds(23, 188, 647, 240);
-		contentPanel.add(scrollPane);
-		
-		textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
 		
 		label = new JLabel("X");
 		label.addMouseListener(this);
 		label.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		label.setBounds(690, 0, 19, 29);
 		contentPanel.add(label);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(23, 131, 672, 326);
+		contentPanel.add(scrollPane);
+		
+		tblSocio = new JTable();
+		tblSocio.setFillsViewportHeight(true);
+		scrollPane.setViewportView(tblSocio);
+		
+		modelo = new DefaultTableModel();
+        modelo.addColumn("Código");
+        modelo.addColumn("Nombres");
+        modelo.addColumn("Apellidos");
+        modelo.addColumn("Teléfono");
+        modelo.addColumn("DNI");
+        tblSocio.setModel(modelo);
+ 
+ 
+ 
+        modelo.setRowCount(0);
+        Object[] fila = { 10, 20, 30, 40, 50 };
+        modelo.addRow(fila);
+
+        habilitarEntradas(false);
+        ajustarAnchoColumnas();
+        listar();
+        editarFila();
 	}
+	//Declaración global
+	ArregloSocio as = new ArregloSocio();
+	
 	public void actionPerformed(ActionEvent arg0) {
-		if (arg0.getSource() == btnListar) {
+		if (arg0.getSource() == btnAdicionar) {
 			actionPerformedBtnListar(arg0);
 		}
-		if (arg0.getSource() == btnConsultar) {
-			actionPerformedButton(arg0);
-		}
-	}
-	protected void actionPerformedButton(ActionEvent arg0) {
 	}
 	public void mouseClicked(MouseEvent e) {
 		if (e.getSource() == label) {
@@ -166,6 +190,18 @@ public class SOCIO extends JDialog implements ActionListener, MouseListener {
 		}
 	}
 	public void mouseEntered(MouseEvent e) {
+		if (arg0.getSource() == btnAceptar) {
+			mouseEnteredBtnAceptar(arg0);
+		}
+		if (arg0.getSource() == btnEliminar) {
+			mouseEnteredBtnEliminar(arg0);
+		}
+		if (arg0.getSource() == btnModificar) {
+			mouseEnteredBtnModificar(arg0);
+		}
+		if (arg0.getSource() == btnAdicionar) {
+			mouseEnteredBtnAdicionar(arg0);
+		}
 	}
 	public void mouseExited(MouseEvent e) {
 	}
@@ -173,10 +209,118 @@ public class SOCIO extends JDialog implements ActionListener, MouseListener {
 	}
 	public void mouseReleased(MouseEvent e) {
 	}
-	protected void mouseClickedLabel(MouseEvent e) 
-	{  dispose();
+	protected void mouseClickedTblSocio(MouseEvent arg0) {
+		habilitarEntradas(false);
+		habilitarBotones(true);
+		editarFila();
 	}
-	protected void actionPerformedBtnListar(ActionEvent arg0) {
+	
+	
+	protected void mouseEnteredBtnAdicionar(MouseEvent arg0) {
+		btnAdicionar.setCursor(new Cursor(Cursor.HAND_CURSOR));
 	}
+	protected void mouseEnteredBtnModificar(MouseEvent arg0) {
+		btnModificar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+	}
+	protected void mouseEnteredBtnEliminar(MouseEvent arg0) {
+		btnEliminar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+	}
+	protected void mouseEnteredBtnAceptar(MouseEvent arg0) {
+		btnAceptar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+	}
+	//Metodos tipo void (sin parametros)
+	void ajustarAnchoColumnas() {
+		TableColumnModel tcm = tblSocio.getColumnModel();
+		tcm.getColumn(0).setPreferredWidth(anchoColumna(20));
+		tcm.getColumn(1).setPreferredWidth(anchoColumna(20));
+		tcm.getColumn(2).setPreferredWidth(anchoColumna(20));
+		tcm.getColumn(3).setPreferredWidth(anchoColumna(20));
+		tcm.getColumn(4).setPreferredWidth(anchoColumna(20));
+	}
+	void listar() {
+		int posFila = 0;
+		if(modelo.getRowCount()>0)
+			posFila= tblSocio.getSelectedRow();
+		if(modelo.getRowCount() == as.tamaño() -1)
+			posFila = as.tamaño() - 1;
+		if(posFila == as.tamaño())
+			posFila --;
+		modelo.setRowCount(0);
+		Socio p;
+		for (int i=0; i<as.tamaño();i++) {
+			p = as.obtener(i);
+			Object[] fila = { p.getCodigoSocio(),
+							p.getNombres(),
+							p.getApellidos(),
+							p.getTelefono(),
+							p.getDni()};
+			modelo.addRow(fila);
+			}
+			if(as.tamaño()>0)
+				tblSocio.getSelectionModel().setSelectionInterval(posFila, posFila);
+		}
+	void editarFila() {
+		if (as.tamaño()==0)
+			limpieza();
+		else {
+			Socio s= as.obtener(tblSocio.getSelectedRow());
+			txtCodigoSocio.setText("" + s.getCodigoSocio());
+			txtNombre.setText(s.getNombres());;
+			txtApellido.setText(s.getApellidos());
+			txtTelefono.setText(s.getTelefono());
+			txtDni.setText(s.getDni());
+		}
+	}
+	void limpieza() {
+		txtCodigoSocio.setText("" + as.codigoCorrelativo());
+		txtNombre.setText("");;
+		txtApellido.setText("");
+		txtTelefono.setText("");
+		txtDni.setText("");
+	}
+	//Metodos tipo void (con parametros)
+	void mensaje (String s) {
+		JOptionPane.showMessageDialog(this, s, "Información", 0);
+	}
+	void error(String s, JTextField txt) {
+		mensaje(s);
+		txt.setText("");
+		txt.requestFocus();
+	}
+	void habilitarEntradas(boolean sino) {
+		txtNombre.setEditable(sino);
+		txtApellido.setEditable(sino);
+		txtTelefono.setEditable(sino);
+		txtDni.setEditable(sino);
+	}
+	void habilitarBotones(boolean sino) {
+		btnAdicionar.setEnabled(sino);
+		btnModificar.setEnabled(sino);
+	}
+//  Métodos que retornan valor (sin parámetros)
+	int leerCodigoSocio() {
+		return Integer.parseInt(txtCodigoSocio.getText().trim());
+	}
+	String leerNombres() {
+		return txtNombre.getText().trim();
+	}
+	String leerApellidos() {
+		return txtApellido.getText().trim();
+	}
+	String leerTelefono() {
+		return txtTelefono.getText().trim();
+	}
+	String leerDni() {
+		return txtDni.getText().trim();
+	}
+	//  Métodos que retornan valor (con parámetros)
+	int anchoColumna(int porcentaje) {
+		return porcentaje * scrollPane.getWidth() / 100;
+	}
+	int confirmar(String s) {
+		return JOptionPane.showConfirmDialog(this, s, "Alerta", 0, 1, null);
+	}
+	
 }
+
 /*Arturo Xulls*/
