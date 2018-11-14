2e04ed4d6be27663e6002ba7e8054a770bdc94e9
JORGE <JORGE@DESKTOP-0LUUNGR>
2018-11-14 02:03:40 -0500

    METODO 5 %

---
+++ HOTEL/src/Clases/Producto.java
@@ -0,0 +1,17 @@
+package Clases;
+
+public class Producto {
+	private int codigoProducto, stock;
+	private String detalle;
+	private double precioUnitario;
+	
+	/*metodos*/
+	
+	public Producto() {
+	this.codigoProducto = codigoProducto;
+	this.stock = stock;
+	this.detalle = detalle;
+	
+	
+	}
+}
