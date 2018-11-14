1a0f07938848d30ca7beb0b33b585ce302742354
ArturoDA <diazarcaarturo@gmail.com>
2018-11-14 03:43:45 -0500

    Arturo ArregloSocio 14/11 03:43am

--- HOTEL/src/Arreglos/ArregloSocio.java
+++ HOTEL/src/Arreglos/ArregloSocio.java
@@ -1,4 +1,6 @@
 package Arreglos;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -53,8 +55,36 @@ public class ArregloSocio {
 			}
 		}
 		private void cargarSocio() {
+			try {
+				BufferedReader br;
+				String linea;
+				String[] s;
+				int codigoSocio;
+				String nombre, apellidos, telefono, dni;
+				br = new BufferedReader(new FileReader("socios.txt"));
+				while ((linea = br.readLine()) != null) {
+					s = linea.split(";");
+					codigoSocio = Integer.parseInt(s[0].trim());
+					nombre = s[1].trim();
+					apellidos = s[2].trim();
+					telefono = s[3].trim();
+					dni = s[4].trim();
+					adicionar(new Socio(codigoSocio, nombre, apellidos, telefono, dni));
+				}
+				br.close();
+			}
+			catch (Exception e) {
+			}
 			
 		}
-	
+	//Operaciones publicas complementarias
+		public int codigoCorrelativo() {
+			if (tamaño() == 0)
+				return 20001;
+			else {
+				int ultimoCodigo = obtener(tamaño()-1).getCodigoSocio();
+				return ultimoCodigo + 1;
+			}
+		}
 
 }
